255ac38cd9f670e5951a76f4d98a99a026044506
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-01-18 02:43:31 +0300

    Delete packages/database/src/client.ts

--- packages/database/src/client.ts
+++
@@ -1,6 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr';
-import type { Database } from './types_db';
-
-export function createClient(supabaseUrl: string, supabaseKey: string) {
-  return createBrowserClient<Database>(supabaseUrl, supabaseKey);
-}
