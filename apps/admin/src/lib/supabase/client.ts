8f24329a90555ee59eb7b259d72cc0ae9744dab0
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-01-19 03:14:36 +0300

    Update client.ts

--- apps/admin/src/lib/supabase/client.ts
+++ apps/admin/src/lib/supabase/client.ts
@@ -1,8 +1,9 @@
-import { createClient as createBaseClient } from '@repo/database/client';
 import { adminEnv } from '@repo/env/admin';
+import { createBrowserClient } from '@supabase/ssr';
+import type { Database } from '@repo/database/types';
 
 export function createClient() {
-  return createBaseClient(
+  return createBrowserClient<Database>(
     adminEnv.NEXT_PUBLIC_SUPABASE_URL,
     adminEnv.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   );
